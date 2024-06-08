bcc9732356adb4340f76383e811455163e8ac591
Willys12 <ben.willys92@gmail.com>
2024-06-08 13:12:13 -0700

    Updated index.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,14 @@ and sets up teardown methods and server configuration.
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 # Initialize Flask application
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ returning a JSON response indicating the service status.
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
     This route returns a JSON response indicating that
@@ -14,3 +14,23 @@ def status():
     Example response: {"status": "OK"}
     """
     return jsonify({"status": "Ok"})
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def stat():
+    """
+    Stats route.
+    This route retrieves the count of each object type
+    and returns a JSON response.
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    response = jsonify(stats)
+    response.status_code = 200
+
+    return response
