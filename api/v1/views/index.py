11068a16289f62cf76bb9905a8bf4c4271f5c7da
Willys12 <ben.willys92@gmail.com>
2024-06-08 13:40:42 -0700

    Index.py update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,12 +6,6 @@ returning a JSON response indicating the service status.
 from api.v1.views import app_views
 from flask import jsonify
 from from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
@@ -22,7 +16,7 @@ def status():
     """
     return jsonify({"status": "Ok"})
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stat', methods=['GET'], strict_slashes=False)
 def stat():
     """
     Stats route.
