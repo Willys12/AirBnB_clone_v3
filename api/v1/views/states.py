3a2cc4e3cc29cfe8eedb97d1d5a0e81de2c4242e
Willys12 <ben.willys92@gmail.com>
2024-06-09 06:55:39 -0700

    States.py

---
+++ api/v1/views/states.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+State objects view.
+This module handles all default RESTFul API actions for State objects.
+"""
+
+from flask import jsonify, request, abort
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route('/states/', methods=['GET'], strict_slashes=False)
+def get_states():
+"""Retrieves the list of all state objects."""
+all_states = storage.all(State).values()
+state_list = [state.to_dict() for in all_state]
+return jsonify(state_list)
+
+@app_views.route('/states/<state_id>' methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+"""Retrieves a State object by its ID."""
+state = storage.get(State, state_id)
+if not state:
+abort(404)
+return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+"""Deletes a State object by its ID."""
+state = storage.get(State, state_id)
+if not state:
+abort(404)
+storage.delete(state)
+storage.save()
+return jsonify({}), 200
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+"""Creates a new State object"""
+if not request.join:
+abort(400, 'Not a JSON')
+if 'name' not in request.json:
+abort(400, 'Missing name')
+data = request.get_json()
+new_state = State(**data)
+new_state.save()
+return jsonify(new_state.to_dict()), 201
+
+@app_views.route('/states/<stte_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+"""Updates a State object by its ID"""
+state = storage.get(State, state_id)
+if not state:
+abort(404)
+if not request.json:
+abort(400, 'Not a JSON')
+data = request.get_json()
+ignored_keys = ['id', 'created_at', 'updated_at']
+for key, value data.items():
+if key not in ignored_keys:
+setattr(sate, key, value)
+state.save()
+return jsonify(state.to_dict()), 200
