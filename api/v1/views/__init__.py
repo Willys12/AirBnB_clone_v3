b5356fd842cebdc4e6a06b2c8c0b47dc20ada4f9
Willys12 <ben.willys92@gmail.com>
2024-06-11 16:59:45 +0300

    Documentation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,17 +5,10 @@ imports all routes from the index module
 """
 from flask import Blueprint
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 # Import all routes from index module
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-app_views.register_blueprint(index_routes, url_prefix='/index')
-app_views.register_blueprint(states_routes, url_prefix='/states')
-app_views.register_blueprint(amenities_routes, url_prefix='/amenities')
-app_views.register_blueprint(cities_routes, url_prefix='/cities')
